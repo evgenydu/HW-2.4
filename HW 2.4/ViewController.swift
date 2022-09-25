01fc9ac3579f463e179fe847cc15c15d7c3db3d3
Evgeny D <ktrp@me.com>
2022-09-25 23:44:10 +0300

    only logic

--- HW 2.4/ViewController.swift
+++ HW 2.4/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var viewMain: UIView!
+    @IBOutlet var valueRed: UILabel!
+    @IBOutlet var valueGreen: UILabel!
+    @IBOutlet var valueBlue: UILabel!
+    @IBOutlet var sliderRed: UISlider!
+    @IBOutlet var sliderGreen: UISlider!
+    @IBOutlet var sliderBlue: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewMain.layer.cornerRadius = 10
+        
     }
 
-
+    @IBAction func setupRedValue(_ sender: UISlider) {
+        valueRed.text = sliderRed.value.formatted()
+        viewMain.backgroundColor = UIColor(red: CGFloat(sliderRed.value), green: CGFloat(sliderGreen.value), blue: CGFloat(sliderBlue.value), alpha: 1)
+    }
+    
+    @IBAction func setupGreenValue(_ sender: UISlider) {
+        valueGreen.text = sliderGreen.value.formatted()
+        viewMain.backgroundColor = UIColor(red: CGFloat(sliderRed.value), green: CGFloat(sliderGreen.value), blue: CGFloat(sliderBlue.value), alpha: 1)
+    }
+    
+    @IBAction func setupBlueValue(_ sender: UISlider) {
+        valueBlue.text = sliderBlue.value.formatted()
+        viewMain.backgroundColor = UIColor(red: CGFloat(sliderRed.value), green: CGFloat(sliderGreen.value), blue: CGFloat(sliderBlue.value), alpha: 1)
+    }
 }
 
