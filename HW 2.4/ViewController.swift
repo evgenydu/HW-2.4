f8c5f624a55e849f0e023576578145a437c4085d
Evgeny D <ktrp@me.com>
2022-09-25 18:54:58 +0300

    Initial Commit

---
+++ HW 2.4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 2.4
+//
+//  Created by Евгений Д on 25.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
